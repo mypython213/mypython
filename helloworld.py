985fe83d4d993b19fcbfdd73bd86f35fee7aa06c
mypytjon <avtoosago@mail.ru>
2019-09-09 12:05:19 +0700

    helloworld

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,20 @@
-hell = 111
+hell = """dfsfs 
+       dfsfsdfs 
+       fdsfsf
+       """
+# ** base types
+int_var = 1
+float_var = 1.0
+bool_var = False
+string_var = "text"
+
+# ** base conversion
+bool(string_var)
+float(int_var)
+int(float_var)
+str(float_var)
+
+
 
 print(hell)
+print(str(float_var))
